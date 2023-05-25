72bfa7a9670960da49b94ef263c6ac9abbcf8fc6
sdh20282 <sdh20282@gmail.com>
2023-05-25 12:03:44 +0900

    Refactor : React import 제거

--- src/components/card/boardDetailCard/BoardDetailCard.jsx
+++ src/components/card/boardDetailCard/BoardDetailCard.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import * as Styled from './style'
 import { useSelector, useDispatch } from 'react-redux'
 
--- src/components/card/boardWriteCard/BoardWriteCard.jsx
+++ src/components/card/boardWriteCard/BoardWriteCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import * as Styled from './style'
 import FormButton from 'components/button/formButton/FormButton';
 import { useSelector, useDispatch } from 'react-redux';
